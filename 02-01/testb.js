8b43fbeb25781bed1f4bf4de5e8c2f1f2ee2dc1d
jihyeonjeong11 <wjdwlgus11@gmail.com>
2019-02-01 15:41:50 +0900

    my til for 2 days

---
+++ 02-01/testb.js
@@ -0,0 +1,25 @@
+
+/**
+ * 
+ * @param {*} name user
+ * @param {*} age 15
+ */
+function a (name, age) {
+
+    return 'hello, ' + name;
+}
+
+const a = 3;
+
+
+ console.log("a ", a);
+ 
+
+
+[{ username:1
+
+}, { username:2
+
+}, { username:3
+
+}]
